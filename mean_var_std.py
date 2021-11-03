e29b7635a1c5730324696d7a87aee308aebcf49a
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-11-03 07:25:52 +0700

    done

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,23 @@
 import numpy as np
 
-def calculate(list):
 
+def calculate(arr: list) -> dict:
+    if len(arr) < 9:
+        raise ValueError('List must contain nine numbers.')
 
+    np_arr = np.array(arr).reshape(3, 3)
+    results = {}
 
+    datas = {
+        'mean': np_arr.mean,
+        'variance': np_arr.var,
+        'standard deviation': np_arr.std,
+        'max': np_arr.max,
+        'min': np_arr.min,
+        'sum': np_arr.sum,
+    }
 
-    return calculations
+    for key, data in datas.items():
+        results[key] = [i.tolist() for i in (data(axis=0), data(axis=1))] + [data()]
+
+    return results
